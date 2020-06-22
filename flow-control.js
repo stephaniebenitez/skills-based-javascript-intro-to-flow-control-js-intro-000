cb381b6739597dfb5ac6847f0be9c84ab2535871
stephanie benitez <stephaniebee94@gmail.com>
2020-06-22 08:59:01 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -11,12 +11,9 @@ else return "you are not a teenager";
 
 
 function ageChecker(age) {
-    if (age >= 13 && age <= 19)
-    return "You are a teenager!";
-  else if ( age <= 12)
-  return "You are a kid";
-  else (age > 19)
-  return "You are a grownup";
+    if (age >= 13 && age <= 19) return "You are a teenager!";
+  else if ( age <= 12)  return "You are a kid";
+  else (age > 19)   return "You are a grownup";
 }
 
 function ternaryTeenager(age) {
